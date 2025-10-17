66858aa85f9554b703d69f70aa2664486bd27660
Al Muksid <deveelopment.almuksid@gmail.com>
2025-10-17 14:24:14 +0600

    Call Back Function

---
+++ README.md
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+<!-- for each function -->
+arr.forEach(function (el, ind) { <!--method -->
+  console.log(el, ind);
+});
+<!-- Desc:create for each function - Function call as a parameter - then create new prametter(el) inside function- # This function is treated as a method. -->
+
+arr.forEach((el, ind) => console.log(el, ind)); // # el- element[arr value] # ind- index[index number]
+
+// ------------------------------------
--- obj.js
+++ obj.js
@@ -1,11 +1,11 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Call back function
 
-// for each function 
-arr.forEach(function (el, ind) { //method
-  console.log(el, ind);
-});
-// Desc:create for each function - Function call as a parameter - then create new prametter(el) inside function- # This function is treated as a method.
+function sayName(){
+    console.log("Al Muksid");
+}
 
-arr.forEach((el, ind) => console.log(el, ind)); // # el- element[arr value] # ind- index[index number]
+function callSay(func){
+    func();
+}
 
-// ------------------------------------
+sayName(callSay)    // CallBack function
